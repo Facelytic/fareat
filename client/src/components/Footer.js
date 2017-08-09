f0be5e83dd7d92bc4b1f6a4fe8210f93183ffa1b
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-08-09 20:12:24 +0700

    footer updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,14 +6,13 @@ const Footer = () => {
       <div className="columns">
         <div className="column is-12">
           <p className="getintouch">Get in touch! We`d love to hear from you.</p>
-
           <div className="columns">
             <div className="column is-5 is-offset-1">
-              <h3>Email: </h3>
+              <h3 className="fa fa-envelope"> &nbsp;Email: </h3>
               <p>fareat@mail.com</p>
             </div>
             <div className="column is-5">
-              <h3>Address: </h3>
+              <h3 className="fa fa-map-marker">&nbsp;Address: </h3>
               <p>Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240</p>
             </div>
           </div>
