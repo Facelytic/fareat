fc5e8f8187ebdd1aa1f367ed5040d9338e360e80
sidikriders <sidik.riders@gmail.com>
2017-08-09 18:02:26 +0700

    home page sudah 50% lah ya

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
+import * as firebase from 'firebase'
 
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Home from './components/Home'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currUser: "Sidik",
+      classList: [ "Dead Fox", "Ethopian Fox", "Fire Fox", "Grey Fox", "Happy Fox", "Island Fox" ],
+      subjectList: [ "VueJS", "API", "React", "Express", "Sequelize"],
+      pertemuanList: [ 1, 2, 3, 4, 5, 6, 7],
+      hasilGo: ""
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Home></Home>
       </div>
     );
+  }
+
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyDi5dB55TluOsc1Vw--bru8tJvN1Suv1r0",
+      authDomain: "freat-7b322.firebaseapp.com",
+      databaseURL: "https://freat-7b322.firebaseio.com",
+      projectId: "freat-7b322",
+      storageBucket: "freat-7b322.appspot.com",
+      messagingSenderId: "514164172565"
+    };
+    firebase.initializeApp(config);
   }
 }
 
