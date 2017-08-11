6a93aa404a73d60c29b23150b4f274223219e24c
1000pilar <nunua7a8@gmail.com>
2017-08-12 00:10:03 +0700

    berhasil update object by student_id

--- server/controllers/absents-cont.js
+++ server/controllers/absents-cont.js
@@ -57,32 +57,33 @@ module.exports = {
     })
   },
 
-
   update: (req, res)=>{
     Absent.findOne({student_list: {$elemMatch: {student_id:req.params.student_id}}}, (err, result)=>{
       if(!err){
+        var index = result.student_list.filter( (x) => {return x.student_id == req.params.student_id})
+        console.log(index);
         var query = {
-          student_list: [{
-            student_id: req.params.student_id,
-            pertemuan_1: req.body.pertemuan_1 || result.student_list.pertemuan_1,
-            pertemuan_2: req.body.pertemuan_2 || result.student_list.pertemuan_2,
-            pertemuan_3: req.body.pertemuan_3 || result.student_list.pertemuan_3,
-            pertemuan_4: req.body.pertemuan_4 || result.student_list.pertemuan_4,
-            pertemuan_5: req.body.pertemuan_5 || result.student_list.pertemuan_5,
-            pertemuan_6: req.body.pertemuan_6 || result.student_list.pertemuan_6,
-            pertemuan_7: req.body.pertemuan_7 || result.student_list.pertemuan_7
-          }],
-          subject: result.subject,
-          class_name: result.class_name,
-          user_id: result.user_id
-        }
-        Absent.findOneAndUpdate({student_list: {$elemMatch: {student_id:req.params.student_id}}}, {$set: query}, {new: true}, (err, data)=>{
+            student_id: index[0].student_id,
+            pertemuan_1: req.body.pertemuan_1 || index[0].pertemuan_1,
+            pertemuan_2: req.body.pertemuan_2 || index[0].pertemuan_2,
+            pertemuan_3: req.body.pertemuan_3 || index[0].pertemuan_3,
+            pertemuan_4: req.body.pertemuan_4 || index[0].pertemuan_4,
+            pertemuan_5: req.body.pertemuan_5 || index[0].pertemuan_5,
+            pertemuan_6: req.body.pertemuan_6 || index[0].pertemuan_6,
+            pertemuan_7: req.body.pertemuan_7 || index[0].pertemuan_7
+          }
+        Absent.findOneAndUpdate({student_list: {$elemMatch: {student_id:req.params.student_id}}}, {$set: {"student_list.$": query,
+        subject: result.subject,
+        class_name: result.class_name,
+        user_id: result.user_id}}, {new: true}, (err, data)=>{
           if(!err) {
             res.status(200).send(data)
           } else {
             res.status(400).send(err)
           }
         })
+      } else {
+        res.status(400).send(err)
       }
     })
   },
