90319e329289105ce4d92785fc962ffda1e5a7af
sidikriders <your@example.com>
2017-08-16 11:41:52 +0700

    commit all files

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div style={styles.header}>
       <nav className="navbar" style={{width: "80%", margin: "auto"}}>
         <div className="navbar-brand">
-          <a className="navbar-item" href="http://bulma.io">
-            <img src="https://app.getresponse.com/images/common/templates/landing/241/1/img/241_01.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+          <Link className="navbar-item" to="/home">
+            <img src="http://i.imgur.com/T5JKfhD.png" alt="FACELYTIC - fase fase, litik litik" height="28" />
             {/* <p>LOGO</p> */}
-          </a>
+          </Link>
           <div className="navbar-burger burger" data-target="navMenuExample">
             <span></span>
             <span></span>
